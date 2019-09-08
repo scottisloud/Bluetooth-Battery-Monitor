98f6963c47dd5e1b162e8cf66e3b9fc94c502c1e
Scott Lougheed <scott@scottlougheed.com>
2019-09-08 09:45:04 -0400

    Initial Commit

---
+++ Bluetooth Battery Monitor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Bluetooth Battery Monitor
+//
+//  Created by Scott Lougheed on 2019/9/8.
+//  Copyright © 2019 Scott Lougheed. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Bluetooth Battery Monitor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Bluetooth Battery Monitor
+//
+//  Created by Scott Lougheed on 2019/9/8.
+//  Copyright © 2019 Scott Lougheed. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
